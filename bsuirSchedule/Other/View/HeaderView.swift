57449271d537d436b6aa0a5b4b7b24af8faf8259
Hurynovič Andrej <dpkdpk10@icloud.com>
2023-04-30 16:35:07 +0300

    HeaderViewWithArrow

--- bsuirSchedule/Other/View/HeaderView.swift
+++ bsuirSchedule/Other/View/HeaderView.swift
@@ -9,22 +9,33 @@ import SwiftUI
 
 struct HeaderView: View {
     var title: String
+    var withArrow: Bool
     
-    init(_ title: String) {
+    init(_ title: String, withArrow: Bool = false ) {
         self.title = title
+        self.withArrow = withArrow
     }
     
     var body: some View {
-        Text(title)
-            .font(.title2)
-            .fontWeight(.heavy)
-            .foregroundColor(.primary)
-            .padding(.top)
+        HStack {
+            Text(title)
+                .fontWeight(.heavy)
+                .foregroundColor(.primary)
+            if withArrow {
+                Text(Image(systemName: "chevron.right"))
+                    .foregroundColor(.gray)
+            }
+        }
+        .font(.title2)
+        .padding(.top)
     }
 }
 
 struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView("ВМСиС")
+        VStack {
+            HeaderView("ВМСиС", withArrow: false)
+            HeaderView("ВМСиС", withArrow: true)
+        }
     }
 }
