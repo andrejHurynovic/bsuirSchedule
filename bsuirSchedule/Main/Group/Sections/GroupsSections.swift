b922587e1b1583c8cb8b897b8e6334826dc0393b
Hurynovič Andrej <dpkdpk10@icloud.com>
2023-05-10 23:33:15 +0300

    Group sp fix

--- bsuirSchedule/Main/Group/Sections/GroupsSections.swift
+++ bsuirSchedule/Main/Group/Sections/GroupsSections.swift
@@ -22,11 +22,20 @@ extension Sequence where Element == Group {
     }
     
     private func specialitySections(useSpecialityName: Bool = false) -> [NSManagedObjectsSection<Group>] {
-        self.sectioned(by: \.speciality)
-            .sorted { $0.key?.formattedID() ?? "" < $1.key?.formattedID() ?? "" }
-            .map { NSManagedObjectsSection(title: $0.key?.formattedDescription(useSpecialityName: useSpecialityName) ?? "Без специальности",
-                                           id: $0.key?.objectID.description ?? "",
-                                           items: $0.value) }
+        var sections: [NSManagedObjectsSection<Group>] = []
+        var sectionedGroups = self.sectioned(by: \.speciality)
+        if let noSpecialityGroups = sectionedGroups.removeValue(forKey: nil) {
+            sections.append( NSManagedObjectsSection(title: "Без специальности",
+                                                     id: "",
+                                                     items: noSpecialityGroups) )
+        }
+        sections.insert(contentsOf:
+                            sectionedGroups.sorted { $0.key!.formattedID() < $1.key!.formattedID() }
+            .map { NSManagedObjectsSection(title: $0.key!.formattedDescription(useSpecialityName: useSpecialityName),
+                                           id: $0.key!.objectID.description,
+                                           items: $0.value) },
+                        at: 0)
+        return sections
     }
     
     private func facultySections() -> [NSManagedObjectsSection<Group>] {
